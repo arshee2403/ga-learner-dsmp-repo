4b65bb7f86baf60f0a656d57548968b2d3d00e34
arshee2403 <arshee2403@users.noreply.github.com>
2019-06-26 11:59:06 +0530

    Code file created.

---
+++ Superhero-Statistics/code.py
@@ -0,0 +1,75 @@
+# --------------
+#Header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#path of the data file- path
+data = pd.read_csv(path)
+data['Gender'].replace('-','Agender',inplace=True)
+gender_count = data['Gender'].value_counts()
+print(gender_count)
+gender_count.plot(kind='bar')
+
+#Code starts here 
+
+
+
+
+# --------------
+#Code starts here
+alignment = data['Alignment'].value_counts()
+plt.pie(alignment)
+plt.title('Character Alignment')
+plt.show()
+print(alignment)
+
+
+# --------------
+#Code starts here
+sc_df = data[['Strength','Combat']]
+sc_covariance = sc_df.cov().iloc[0,1]
+sc_strength = sc_df['Strength'].std()
+
+sc_combat = sc_df['Combat'].std()
+sc_pearson = (sc_covariance/(sc_strength*sc_combat))
+print(sc_covariance)
+print(sc_pearson)
+ic_df = data[['Intelligence','Combat']]
+ic_covariance = ic_df.cov().iloc[0,1]
+ic_intelligence = ic_df['Intelligence'].std()
+ic_combat = ic_df['Combat'].std()
+ic_pearson = (ic_covariance/(ic_intelligence*ic_combat))
+print(ic_pearson)
+
+
+# Calulating Pearson correlation coefficient
+
+
+
+# --------------
+#Code starts here
+total_high = data['Total'].quantile(0.99)
+super_best = data[data['Total']>total_high]
+super_best_names = list(super_best['Name'])
+print(super_best_names)
+
+
+# --------------
+#Code starts here
+# Initialize figure and axes
+fig,(ax_1,ax_2,ax_3) = plt.subplots(1,3)
+
+
+ax_1.boxplot(data['Intelligence'])
+ax_1.set_title('Intelligence')
+
+ax_2.boxplot(data['Speed'])
+ax_2.set_title('Speed')
+
+ax_3.boxplot(data['Power'])
+ax_3.set_title('Power')
+plt.show()
+
+
